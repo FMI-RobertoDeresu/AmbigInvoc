a808eec0170c5a79d8e2102fd894545776bead45
Roberto Deresu <roberto.deresu@gmail.com>
2017-07-28 14:28:24 +0300

    Add project files.

---
+++ src/AmbigInvoc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AmbigInvoc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SomeMethod(GetObject);
+        }
+
+        public static IInterface GetObject() => null;
+
+        public static void SomeMethod(Func<IInterface> func) => Console.WriteLine("First");
+
+        public static void SomeMethod(Func<Task<IInterface>> funcAsync) => Console.WriteLine("Second");
+    }
+
+    public interface IInterface { }
+}
